70931438f10f82c63ea5d2b8c5036eeed59f12c9
Christofer Mayon <125940522+ChristoferMayon@users.noreply.github.com>
2025-06-27 13:24:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -30,9 +30,9 @@ function enviarMensagem() {
   fetch(`https://api.z-api.io/instances/${instanceId}/token/${instanceToken}/send-button-actions`, {
     method: "POST",
     headers: {
-  "Content-Type": "application/json",
-  "Authorization": `Bearer ${instanceToken}`
-}
+      "Content-Type": "application/json",
+      "Client-Token": instanceToken // ✅ cabeçalho necessário
+    },
     body: JSON.stringify(payload)
   })
   .then(response => response.json())
